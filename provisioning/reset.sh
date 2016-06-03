554f57db57c2e9dada50acec5e527d8191b97c0f
Stephen Ball <stephen@rebelinblue.com>
2016-06-03 22:44:47 +0100

    Added an alias to reset everything

--- provisioning/reset.sh
+++ provisioning/reset.sh
@@ -6,3 +6,8 @@ mysql -udeployer -psecret -e "DROP DATABASE IF EXISTS deployer;"
 mysql -udeployer -psecret -e "CREATE DATABASE deployer DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci;"
 cd /var/www/deployer
 php artisan app:reset
+sudo service supervisor restart
+redis-cli FLUSHALL
+sudo service redis-server restart
+sudo service beanstalkd restart
+
