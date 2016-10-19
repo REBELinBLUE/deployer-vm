6b94d1b853c620a58d6fd57b0f67aab912a9c2c4
Stephen Ball <stephen.ball@sainsburys.co.uk>
2016-10-19 13:31:12 +0100

    Added yarn to path

--- provisioning/profile.sh
+++ provisioning/profile.sh
@@ -19,7 +19,7 @@ fi
 
 # Update .profile
 echo '' >> /home/vagrant/.profile
-echo 'export PATH=/var/www/deployer/vendor/bin:$PATH' >> /home/vagrant/.profile
+echo 'export PATH=$HOME/.yarn/bin:/var/www/deployer/vendor/bin:$PATH' >> /home/vagrant/.profile
 echo '' >> /home/vagrant/.profile
 echo 'alias php="php -dzend_extension=xdebug.so"' >> /home/vagrant/.profile
 echo 'alias artisan="php artisan"' >> /home/vagrant/.profile
