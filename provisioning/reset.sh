612b5e9682f2d0e785f6487eb561a6e7f784e3be
Stephen Ball <stephen@rebelinblue.com>
2017-01-22 20:51:02 +0000

    Tweak reset script

--- provisioning/reset.sh
+++ provisioning/reset.sh
@@ -6,9 +6,9 @@ mysql -udeployer -psecret -e "DROP DATABASE IF EXISTS deployer;"
 mysql -udeployer -psecret -e "CREATE DATABASE deployer DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_unicode_ci;"
 php /var/www/deployer/artisan app:reset
 
-sudo cp /var/www/deployer/docs/examples/supervisor.conf /etc/supervisor/conf.d/deployer.conf
-sudo cp /var/www/deployer/docs/examples/crontab /etc/cron.d/deployer
-sudo cp /var/www/deployer/docs/examples/nginx.conf /etc/nginx/sites-available/deployer.conf
+sudo cp /var/www/deployer/examples/supervisor.conf /etc/supervisor/conf.d/deployer.conf
+sudo cp /var/www/deployer/examples/crontab /etc/cron.d/deployer
+sudo cp /var/www/deployer/examples/nginx.conf /etc/nginx/sites-available/deployer.conf
 sudo cp /vagrant/provisioning/nginx.conf /etc/nginx/sites-available/beanstalkd.conf
 
 sudo rm -rf /var/www/deployer/storage/{framework,logs,clockwork}/
