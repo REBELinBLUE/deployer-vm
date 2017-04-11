4f3d7f3d25c7666a20679fffd80cf5f62b3be9c2
Stephen Ball <stephen@rebelinblue.com>
2017-04-11 21:07:26 +0100

    Added git-extras

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -34,7 +34,7 @@ curl --silent --location https://deb.nodesource.com/setup_6.x | bash -
 apt-get update -y
 
 # Install Some Basic Packages
-apt-get install -y --allow-unauthenticated build-essential git libmcrypt4 python-pip supervisor unattended-upgrades nano libnotify-bin
+apt-get install -y --allow-unauthenticated build-essential git libmcrypt4 python-pip supervisor unattended-upgrades nano libnotify-bin git-extras
 
 # Set the Timezone
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
